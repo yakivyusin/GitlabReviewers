74e2844800310dfcf2cb8b735f3ee6c750dc0070
Yakiv Yusin <yusin.yakiv@gmail.com>
2019-03-17 15:01:20 +0200

    Added Reviewers Extraction to Merge Request Description

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 var description = document.getElementById("merge_request_description");
 
 if (description) {
-  description.value = "Test";
+  chrome.storage.sync.get({reviewers: ""}, (item) => {
+    description.value = item.reviewers.match(/\w+/g).map(reviewer => `@${reviewer}`).join(" ");
+  });
 }
